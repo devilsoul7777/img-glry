cdc3babaef1c5232fe0c7d97542855a33d4d7030
thedemon <sehaj2122@gmail.com>
2021-08-02 16:08:02 +0530

    code format

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from'react';
+import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
@@ -11,23 +11,23 @@ const App = () => {
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(word);
-    fetch(`https://api.unsplash.com/photos/random?query=${word}&client_id=${UNSPLASH_KEY}`)
+    fetch(
+      `https://api.unsplash.com/photos/random?query=${word}&client_id=${UNSPLASH_KEY}`
+    )
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
       })
       .catch((err) => {
         console.log(err);
-      })
+      });
     setWord('');
-  }
-
+  };
   return (
     <div>
       <Header />
-      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit}/>
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
     </div>
   );
 };
-
 export default App;
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,17 +2,17 @@ import React from 'react';
 import { Navbar, Container } from 'react-bootstrap';
 
 const navbarStyle = {
-    backgroundColor: 'lightblue'
+  backgroundColor: 'lightblue',
 };
 
 const Header = () => {
-    return (
-        <Navbar style={navbarStyle} variant="light">
-            <Container>
-                <Navbar.Brand href="/">Image Gallery</Navbar.Brand>
-            </Container>
-        </Navbar>
-    );
+  return (
+    <Navbar style={navbarStyle} variant="light">
+      <Container>
+        <Navbar.Brand href="/">Image Gallery</Navbar.Brand>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Header;
