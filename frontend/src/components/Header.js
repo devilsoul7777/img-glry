e4f814c2f2cd72e1669b676ce992558759b5cf4c
thedemon <sehaj2122@gmail.com>
2021-08-04 10:53:47 +0530

    added text logo

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Navbar, Container } from 'react-bootstrap';
+import { ReactComponent as Logo } from '../images/logo.svg';
 
 const navbarStyle = {
-  backgroundColor: 'lightblue',
+  backgroundColor: 'red',
 };
 
 const Header = () => {
   return (
     <Navbar style={navbarStyle} variant="light">
       <Container>
-        <Navbar.Brand href="/">Image Gallery</Navbar.Brand>
+        <Logo style={{ maxWidth: '18rem', maxHeight: '3rem' }} />
       </Container>
     </Navbar>
   );
